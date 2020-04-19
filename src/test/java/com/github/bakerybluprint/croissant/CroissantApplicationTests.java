2cfd7c51b17d8dd8890cf0ee4e7a01e9727ddcc0
hotire <gngh0101@gmail.com>
2020-04-19 13:28:01 +0900

    init

---
+++ src/test/java/com/github/bakerybluprint/croissant/CroissantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bakerybluprint.croissant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CroissantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
