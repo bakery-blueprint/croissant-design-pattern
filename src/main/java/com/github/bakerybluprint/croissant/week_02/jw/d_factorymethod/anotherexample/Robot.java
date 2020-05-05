0efa52fd4f16143d1bd50e065dc92bb05de025f0
junwoo <33277588+mike6321@users.noreply.github.com>
2020-05-06 01:26:33 +0900

    Source code Commit

---
+++ src/main/java/com/github/bakerybluprint/croissant/week_02/jw/d_factorymethod/anotherexample/Robot.java
@@ -0,0 +1,12 @@
+package com.github.bakerybluprint.croissant.week_02.jw.d_factorymethod.anotherexample;
+
+/**
+ * Project : EffectiveStudy
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/05/06
+ * Time : 12:48 오전
+ */
+public abstract class Robot {
+    public abstract String getName();
+}
---
+++ src/main/java/com/github/bakerybluprint/croissant/week_02/jw/d_factorymethod/concrete/MpPotion.java
@@ -0,0 +1,18 @@
+package com.github.bakerybluprint.croissant.week_02.jw.d_factorymethod.concrete;
+
+
+import com.github.bakerybluprint.croissant.week_02.jw.d_factorymethod.framework.Item;
+
+/**
+ * Project : EffectiveStudy
+ * Created by InteliJ IDE
+ * Developer : junwoochoi
+ * Date : 2020/04/22
+ * Time : 10:46 오후
+ */
+public class MpPotion implements Item {
+    @Override
+    public void use() {
+        System.out.println("마력 회복");
+    }
+}
